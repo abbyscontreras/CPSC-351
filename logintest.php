9b2a7582cd0501520f39159bd1edb0d7c3f6e9ae
abbycontreras <94137911+abbycontreras@users.noreply.github.com>
2022-03-29 18:04:49 -0400

    logintest.php

--- logintest.php
+++ logintest.php
@@ -1,3 +1,28 @@
+<?php
+    include('db_connection.php');
+    global $conn;
+    $msg = "";
+
+    if (isset($_POST['submit'])) {
+        echo "<pre>";
+        print_r($_POST);
+
+        $username = mysqli_real_escape_string($conn, $_POST['username']);
+        $password = mysqli_real_escape_string($conn, $_POST['password']);
+        $sql = mysqli_query($conn, "select * from login where username = '$username' && password = '$password'");
+        $num = mysqli_num_rows($sql);
+        if ($num>0) {
+            $row=mysqli_fetch_assoc($sql);
+            $_SESSION['USER_ID']=$row['id'];
+            $_SESSION['USER_NAME']=$row['username'];
+            header("location:index.php");
+            echo "Found";
+        } else {
+            $msg = "Please enter valid credentials";
+        }
+    }
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -37,11 +62,13 @@
                     <div class="form-outline mb-4">
                         <label for="password">Password</label>
                         <div class="box">
-                            <input type="text" name="password" placeholder="password" class="form-control" required>
+                            <input type="password" name="password" placeholder="password" class="form-control" required>
                         </div>
                     </div>
-                        <button type="button" class="btn btn-dark btn-submit btn-block mb-4">Sign in</button>
-
+                        <input type="submit" name="submit" value="Login" class="btn btn-dark btn-submit btn-block mb-4">
+                    <div class="error">
+                        <?php echo $msg?>
+                    </div>
 
                 </form>
             </div>
