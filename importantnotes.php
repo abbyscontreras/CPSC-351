cf572cee4672f7b6d3336820724528edee926919
elliemiley <eleanor.miley.19@cnu.edu>
2022-04-05 17:31:21 -0400

    important notes updated

---
+++ importantnotes.php
@@ -0,0 +1,47 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <title>Important Notes</title>
+</head>
+<body>
+<h2>Important Notes</h2>
+<table>
+    <tr>
+        <th>Topic</th>
+        <th>Date</th>
+        <th>Notes</th>
+    </tr>
+    <tr>
+        <td>Student: Jack McDonald</td>
+        <td>1/14/22</td>
+        <td>Is doing well, but keep an eye on grades, set up later meetings for help.</td>
+    </tr>
+    <tr>
+        <td>Student: Abby Contreras</td>
+        <td>1/7/22</td>
+        <td>On top of everything, very driven. </td>
+    </tr>
+    <tr>
+        <td>Course Update: CPSC 255</td>
+        <td>12/29/21</td>
+        <td>This course is now being offered in both Fall and Spring semesters, as well as the May Semester.</td>
+    </tr>
+    <tr>
+        <td>New Course Alert: AMST 218</td>
+        <td>12/15/21</td>
+        <td>“Aftermath of World War 1” is now being taught by Jeffrey Morrison in the Spring Semester of 2022.</td>
+    </tr>
+    <tr>
+        <td>Student: Brett Kropinski</td>
+        <td>8/20/21</td>
+        <td>Transferred. Has enough credits but needs to fulfill major reqs.</td>
+    </tr>
+
+</table>
+</body>
+</html>
+
