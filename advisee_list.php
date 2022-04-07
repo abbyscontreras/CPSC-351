3332f48a0f16260ce9f356b85770f1ba39947476
BrettKropinski <brett.kropinski.20@cnu.edu>
2022-04-07 17:39:34 -0400

    images updated

--- advisee_list.php
+++ advisee_list.php
@@ -3,6 +3,7 @@ include('db_connection.php');
 global $conn;
 include('header.php');
 ?>
+    <img src="images/kropinski_cnu_profile_picture.jpeg" alt="Brett Kropinski">
     <a href="brett.html">brett</a>
     <a href="pat.html">pat</a>
     <a href="jack.html">jack</a>
