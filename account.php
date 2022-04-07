b459808fe63b00277e363007d5673373e53b23a2
abbycontreras <94137911+abbycontreras@users.noreply.github.com>
2022-04-07 14:55:15 -0400

    working on account.php

--- account.php
+++ account.php
@@ -4,3 +4,18 @@ global $conn;
 include('header.php');
 ?>
 
+<div class="row">
+    <div class="profileCard">
+        <div class="card">
+            <div class="row">
+                <h2>Admin</h2>
+                <img src="" alt="">
+            </div>
+            <div class="row">
+                <h4>Biography</h4>
+                <h4>Courses Teaching</h4>
+                <h4>Contact Information</h4>
+            </div>
+        </div>
+    </div>
+</div>
