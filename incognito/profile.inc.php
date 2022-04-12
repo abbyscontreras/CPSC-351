fe4103d616b9f764984bd5a75d1f080469d05b31
abbycontreras <94137911+abbycontreras@users.noreply.github.com>
2022-04-12 17:30:17 -0400

    tried insert into blob didnt work

--- incognito/profile.inc.php
+++ incognito/profile.inc.php
@@ -1,9 +1,19 @@
 <?php
 include('db_connection.php');
 global $conn;
-if (isset($_POST['Update']))    {
-    $user = $_SESSION['user'];
-    echo $user;
+if (isset($_POST['update'])) {
+    $semester = $_POST['semester'];
+    $course = $_POST['course'];
+    $credits = $_POST['credits'];
+//    $sql = "update course_schedule set semester = '$semester', credits = '$credits' where course_name = '$course'";
+    if (mysqli_query($conn, $sql)) {
+        echo "<h3>data stored in a database successfully.</h3>";
+    } else {
+        echo "ERROR: Hush! Sorry $sql. "
+            . mysqli_error($conn);
+    }
+
+}
 }
 
 
